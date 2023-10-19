086874ec79d366d0754d2b14d2d4ad79b7f6377d
CHAD-DEV\Medium Chad <cbrathwaite93@gmail.com>
2023-10-18 22:24:23 -0400

    numbers

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿class Hiearth
+﻿using System.Xml;
+
+class Hiearth
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Console.WriteLine("What's your name:");
         string nombre = Console.ReadLine();
+        string number = Console.ReadLine();
+        bool isEven = IsNumberEven(number);
+
+        if(isEven)
+        {
+            nombre = "chad";
+        }
+
         Console.WriteLine("fuck off " + nombre);
+
+        bool IsNumberEven(string strNumber)
+        {
+            int convertedNumber = Convert.ToInt32(strNumber);
+            int remainder = convertedNumber % 2;
+            if (remainder == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
     }
+
 }
